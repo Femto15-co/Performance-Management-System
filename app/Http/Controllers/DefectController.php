77a27a63c0010f1bdb95f5acc7bedb03f70b6ca1
TheV <Ismail>
2017-01-09 20:28:55 +0200

    Create Defects Controller

--- app/Http/Controllers/DefectController.php
+++ app/Http/Controllers/DefectController.php
@@ -5,81 +5,109 @@ namespace App\Http\Controllers;
 use App\Defect;
 use Illuminate\Http\Request;
 
-class DefectController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+class DefectController extends Controller {
+	/**
+	 * Display a listing of the users with defects.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index() {
+		//Include DataTable
+		$includeDataTable = true;
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+		//DataTable ajax route
+		$dataTableRoute = route('report.list');
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+		return view('reports.index', compact('includeDataTable', 'dataTableRoute'));
+	}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Defect  $defect
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Defect $defect)
-    {
-        //
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create() {
+		//
+	}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Defect  $defect
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Defect $defect)
-    {
-        //
-    }
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request) {
+		//
+	}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Defect  $defect
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Defect $defect)
-    {
-        //
-    }
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  \App\Defect  $defect
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show(Defect $defect) {
+		//
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Defect  $defect
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Defect $defect)
-    {
-        //
-    }
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  \App\Defect  $defect
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit(Defect $defect) {
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \App\Defect  $defect
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, Defect $defect) {
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  \App\Defect  $defect
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy(Defect $defect) {
+		//
+	}
+	/**
+	 * Returns reports data to DataTable
+	 *
+	 * @return JSON
+	 */
+	public function listData() {
+		$reports = Report::join('users', 'reports.user_id', '=', 'users.id')
+			->select(['reports.id', 'users.name', 'reports.overall_score', 'reports.created_at']);
+
+		return Datatables::of($reports)
+			->addColumn('action', function ($reports) {
+
+				$formHead = "<form class='form-horizontal main_form' method='POST' action='" . route('report.destroy', $reports->id) . "'>" . csrf_field();
+				$viewLink = "<a href=" . route('report.show', $reports->id) . " class='btn btn-xs btn-success'><i class='glyphicon glyphicon-view'></i> " . trans('general.show') . "</a>&nbsp;";
+				$editLink = "<a href=" . route('report.edit', $reports->id) . " class='btn btn-xs btn-primary'><i class='glyphicon glyphicon-edit'></i>" . trans('general.edit') . "</a>";
+				$deleteForm =
+				"  <input type='hidden' name='_method' value='DELETE'/>
+                        <button type='submit' class='btn btn-xs btn-danger main_delete'>
+                            <i class='glyphicon glyphicon-delete'></i> " . trans('general.delete') . "
+                        </button>
+                    </form>";
+
+				return $formHead . $viewLink . $editLink . $deleteForm;
+
+			}) //Change the Format of report date
+			->editColumn('created_at', function ($reports) {
+				return date('d M Y', strtotime($reports->created_at));
+			}) // To Update the Offdays Section and Convert it to String
+			->make();
+	}
 }
--- routes/web.php
+++ routes/web.php
@@ -9,23 +9,33 @@
 | by your application. Just tell Laravel the URIs it should respond
 | to using a Closure or controller method. Build something great!
 |
-*/
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
 
 Auth::routes();
 
-Route::group(['prefix'=>'report'], function(){
-    Route::get('/data', ['as'=>'report.list', 'uses'=>'ReportController@listData']);
-    Route::get('/', ['as'=>'report.index', 'uses'=>'ReportController@index']);
-    Route::get('create/', ['as'=>'report.create.step1', 'uses'=>'ReportController@create']);
-    Route::get('create/{id}', ['as'=>'report.create.step2', 'uses'=>'ReportController@createStepTwo']);
-    Route::post('create', ['as'=>'report.store', 'uses'=>'ReportController@store']);
-    Route::get('{id}', ['as'=>'report.show', 'uses'=>'ReportController@show']);
-    Route::get('{id}/edit', ['as'=>'report.edit', 'uses'=>'ReportController@edit']);
-    Route::put('{id}', ['as'=>'report.update', 'uses'=>'ReportController@update']);
-    Route::delete('{id}', ['as'=>'report.destroy', 'uses'=>'ReportController@destroy']);
+Route::group(['prefix' => 'report'], function () {
+	Route::get('/data', ['as' => 'report.list', 'uses' => 'ReportController@listData']);
+	Route::get('/', ['as' => 'report.index', 'uses' => 'ReportController@index']);
+	Route::get('create/', ['as' => 'report.create.step1', 'uses' => 'ReportController@create']);
+	Route::get('create/{id}', ['as' => 'report.create.step2', 'uses' => 'ReportController@createStepTwo']);
+	Route::post('create', ['as' => 'report.store', 'uses' => 'ReportController@store']);
+	Route::get('{id}', ['as' => 'report.show', 'uses' => 'ReportController@show']);
+	Route::get('{id}/edit', ['as' => 'report.edit', 'uses' => 'ReportController@edit']);
+	Route::put('{id}', ['as' => 'report.update', 'uses' => 'ReportController@update']);
+	Route::delete('{id}', ['as' => 'report.destroy', 'uses' => 'ReportController@destroy']);
 });
 
+Route::group(['prefix' => 'defect'], function () {
+	Route::get('/data', ['as' => 'defect.list', 'uses' => 'DefectController@listData']);
+	Route::get('/', ['as' => 'defect.index', 'uses' => 'DefectController@index']);
+	Route::get('create', ['as' => 'defect.create', 'uses' => 'DefectController@create']);
+	Route::post('create', ['as' => 'defect.store', 'uses' => 'DefectController@store']);
+	Route::get('{id}', ['as' => 'defect.show', 'uses' => 'DefectController@show']);
+	Route::get('{id}/edit', ['as' => 'defect.edit', 'uses' => 'DefectController@edit']);
+	Route::put('{id}', ['as' => 'defect.update', 'uses' => 'DefectController@update']);
+	Route::delete('{id}', ['as' => 'defect.destroy', 'uses' => 'DefectController@destroy']);
+});
